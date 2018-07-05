5dc0bbe19537fb5699a9a016035a96482fd6c908
birdmod <31164040+birdmod@users.noreply.github.com>
2018-07-06 00:03:52 +0200

    initial commit with input separated by spaces

---
+++ calcul8tor/calcul8tor.App/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace calcul8tor.App.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("calcul8tor.App")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("calcul8tor.App")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("75fe85f4-d73a-48e8-9e4f-48d322886489")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ calcul8tor/calcul8tor.App/Program.fs
@@ -0,0 +1,33 @@
+﻿type Token = 
+| Plus | Minus | Mul | Div | Val of int
+
+let rec tokenize (s:string) : Token = 
+    match s with
+    | "+" -> Plus
+    | "-" -> Minus
+    | "*" -> Mul
+    | "/" -> Div
+    | e -> Val(System.Int32.Parse(e))
+
+let rec eatTokens (tkns: Token list) = 
+    match tkns with
+    | Val(a)::Plus::l -> a + eatTokens l
+    | Val(a)::Minus::l -> a - eatTokens l
+    | Val(a)::Mul::Val(b)::l -> eatTokens (Val(a*b)::l)
+    | Val(a)::Div::Val(b)::l -> eatTokens (Val(a*b)::l)
+    | Val(a)::[] -> a
+    | _ -> failwith "parse error"
+
+[<EntryPoint>]
+let main argv = 
+    let inputWithSpaces:string = System.Console.ReadLine()
+    let tokens = 
+        inputWithSpaces.Split(' ') 
+        |> Array.map tokenize 
+        |> Array.toList
+
+    let res = eatTokens tokens
+
+    printfn "%A" res
+    System.Console.ReadLine |> ignore
+    0 // return an integer exit code
